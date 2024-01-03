d9850ffc174d6354d7ee75dba6a7f1ece6c763de
tsemb012 <tsukasa0171015@gmail.com>
2024-01-03 16:00:52 +0900

    FCS-0001 微修正

--- app/src/main/java/com/tsemb/focusapp/MainActivity.kt
+++ app/src/main/java/com/tsemb/focusapp/MainActivity.kt
@@ -12,32 +12,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tsemb.focusapp.ui.theme.FocusappTheme
 
-// テスト
-
-// TODO Bitriseの導入
-// TODO コードサイズと複雑性分析
-// TODO Danger
-// TODO githubApps
-// TODO klint
-// TODO detekt
+// DONE Bitriseの導入
+// DONE コードサイズと複雑性分析
+// DONE Danger
+// DONE detekt
+// TODO detekt Bitriseに入れる。
 // TODO リリースの自動化
-// TODO セキュリティスキャン
-
+// TODO DeployGateに出す
+// TODO GooglePlayConsoleから出すように
+// TODO テスト導入
 // TODO コードラビットの導入
+// TODO ライブラリ管理を修正
+// TODO マルチモジュール化
+// TODO コミット自動化
+// TODO Hiltの導入
+// TODO KMP化
 
 // TODO GithubActionsを導入
 // TODO Asanaでのプロジェクト管理
 // TODO SonarQube
 
-// TODO マルチモジュール化
-// TODO ライブラリ管理を修正
-// TODO
-// TODO Hiltの導入
-// TODO KMP化
-// TODO テストコード
 // TODO ADHD
 // TODO Ci/CD
-// TODO コミット自動化
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
